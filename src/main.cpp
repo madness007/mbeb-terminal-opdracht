a649bf080d5fed4fcfdaed38b63b880366881311
madness007 <arno.schoutteten@gmail.com>
2022-12-04 13:09:54 +0100

    added threads in main

--- src/main.cpp
+++ src/main.cpp
@@ -4,41 +4,14 @@
 #include "bank.h"
 #include "account.h"
 
-rtos::Thread thread1;
-rtos::Thread thread2;
-rtos::Thread thread3;
-rtos::Thread thread4;
-
-rtos::Semaphore _sem(3);
-int counter = 0;
-
-void client_work(void) {
-    printf("client is working on server\n");
-    counter++;
-    printf("active clients: %d \n", counter);
-    ThisThread::sleep_for(chrono::seconds(rand()%5));
-    counter--;
-}
-
-void connect(void) {
-    _sem.acquire();
-    client_work();
-    _sem.release();
-}
-
-void client() {
-    while (true) {
-        connect();
-        ThisThread::sleep_for(chrono::seconds(rand()%5));
-    }
-}
+rtos::Thread thr_bancontact1;
+rtos::Thread thr_bancontact2;
+rtos::Thread thr_bancontact3;
+rtos::Thread thr_belfius;
+rtos::Thread thr_kbc;
 
 
 int main(void) {
-    thread1.start(client);
-    thread2.start(client);
-    thread3.start(client);
-    thread4.start(client);
 
     Bank::Validation kbc; // Betalingsvalidatie testen (mag dan gebruikt worden in een andere class als dit af is)
     kbc.propose_payment(39.99, 1234, 4555);
