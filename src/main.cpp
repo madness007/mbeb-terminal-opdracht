1495a2040f37780e8ddd86d31408c5c1bfe5cbd8
madness007 <arno.schoutteten@gmail.com>
2022-12-04 14:09:18 +0100

    terminal and terminalserver

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,28 @@
 #include <chrono>
 #include "bank.h"
 #include "account.h"
+#include "terminal.h"
+#include "terminalServer.h"
 
-rtos::Thread thr_bancontact1;
-rtos::Thread thr_bancontact2;
-rtos::Thread thr_bancontact3;
+rtos::Thread thr_terminalServer1;
+rtos::Thread thr_terminalServer2;
+rtos::Thread thr_terminalServer3;
 rtos::Thread thr_belfius;
 rtos::Thread thr_kbc;
 
+Bank::TerminalServer terminalServer1;
+Bank::TerminalServer terminalServer2;
+Bank::TerminalServer terminalServer3;
+
+Bank::Terminal terminal1(&terminalServer1);
+Bank::Terminal terminal2(&terminalServer1);
+Bank::Terminal terminal3(&terminalServer1);
+Bank::Terminal terminal4(&terminalServer2);
+Bank::Terminal terminal5(&terminalServer2);
+Bank::Terminal terminal6(&terminalServer2);
+Bank::Terminal terminal7(&terminalServer3);
+Bank::Terminal terminal8(&terminalServer3);
+Bank::Terminal terminal9(&terminalServer3);
 
 int main(void) {
 
