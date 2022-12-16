90034c147a0be130580475fe72f3262e206a5ebb
madness007 <arno.schoutteten@gmail.com>
2022-12-16 10:42:07 +0100

    included vector

--- src/bank.cpp
+++ src/bank.cpp
@@ -3,20 +3,34 @@
 #include <string>
 
 namespace Banking {
-  
-  Bank::Bank(std::string bankname) : _sem(2)  {
-    this->_bankname = bankname;
+
+  Bank::Bank(std::string bankname) : _sem(2), _vector(1)  {
+    this->bankname = bankname;
   }
 
   int Bank::payment(double amount,  Account * account, Account * to_account) {
     _sem.acquire();   
     if (get_isValid(amount, account) == 0) {
-        account->withdraw_balance(amount);
-        to_account->add_balance(amount); // TODO bank kiezen eerst
+        payment_transfer(amount, account, to_account);
         return 0; //goedgekeurd
     }
     return 1; //niet goedgekeurd
     _sem.release();
+  }
+
+  void Bank::payment_transfer(double amount,  Account * account, Account * to_account) {
+    account->withdraw_balance(amount);
+    if (this->bankname == to_account->bank->bankname) {
+      to_account->add_balance(amount); // TODO bank kiezen eerst
+    }
+    else {
+      struct Message {
+        double amount = amount;
+        Account * account = account;
+        Account * to_account = to_account;
+      };
+      
+    }
   }
 
   int Bank::get_isValid(double amount, Account * account) {
--- src/bank.h
+++ src/bank.h
@@ -3,6 +3,7 @@
 #include "rtos.h"
 #include "account.h"
 #include <string>
+#include <vector>
 
 namespace Banking {
 
@@ -14,14 +15,18 @@ namespace Banking {
     public:
       Bank(std::string bankname);
       int payment(double amount, Account * account, Account * to_account);
+      void payment_transfer(double amount,  Account * account, Account * to_account);
 
     private:
       int get_isValid(double amount, Account * account);
 
     // Attributes (instance variables)
+    public:
+      std::string bankname;
     private:
-      std::string _bankname;
       rtos::Semaphore _sem;
+      vector<int> _vector;
+
       
 
   };
